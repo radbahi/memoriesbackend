f159a36c5769fe3b373a070be51ac99a52cf553c
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-07-27 13:38:59 -0400

    test commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import express from "express"; //for routing
 import mongoose from "mongoose"; //create models
 import cors from "cors"; //enables cross origin requests
+import dotenv from "dotenv"; //MAKE SURE .env IS IN .gitignore
+
+dotenv.config();
 
 //adding "type": "module" in package.json allows us to use ES6 syntax
 
@@ -9,3 +12,19 @@ const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_URL;
+const PORT = process.env.PORT || 5000;
+
+//remember that mongoose returns promises so use .then
+mongoose
+  .connect(CONNECTION_URL, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+  )
+  .catch((error) => console.log(error));
+
+mongoose.set("useFindAndModify", false); //just makes sure we don't get warning in console
