752752dcd8991ebdea2db0a1a9eeb6dfa483b9b9
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-07-28 11:12:24 -0400

    post schema/creation done

--- server/index.js
+++ server/index.js
@@ -2,12 +2,15 @@ import express from "express"; //for routing
 import mongoose from "mongoose"; //create models
 import cors from "cors"; //enables cross origin requests
 import dotenv from "dotenv"; //MAKE SURE .env IS IN .gitignore
+import postRoutes from "./routes/posts.js";
 
 dotenv.config();
 
 //adding "type": "module" in package.json allows us to use ES6 syntax
 
 const app = express();
+
+app.use("/posts", postRoutes);
 
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
