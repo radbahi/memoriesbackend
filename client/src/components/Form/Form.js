6e8741a017cfe91ccad9bf4eb49a7aa3dfade291
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-07-31 12:04:23 -0400

    style files done

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,4 +1,6 @@
+import useStyles from "./styles";
 const Form = () => {
+  const classes = useStyles();
   return <h1>FORM</h1>;
 };
 
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,6 +1,8 @@
 import Post from "./Post/Post";
+import useStyles from "./styles";
 
 const Posts = () => {
+  const classes = useStyles();
   return (
     <>
       <h1>Posts</h1>
