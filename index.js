64ab597269307e57f40e28bed90344480f858c72
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-07-31 12:58:46 -0400

    implemented redux and cors. minor bug fixes

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ dotenv.config();
 
 const app = express();
 
-app.use("/posts", postRoutes);
-
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors());
+app.use(cors()); //this has to be before any defined routes
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL = process.env.MONGO_URL;
 const PORT = process.env.PORT || 5000;
