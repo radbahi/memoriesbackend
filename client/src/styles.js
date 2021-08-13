5be49e4acc701cb646696d63cca2e3da61691ae3
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-08-13 15:35:08 -0400

    updated mobile ui

--- client/src/styles.js
+++ client/src/styles.js
@@ -1,6 +1,6 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-export default makeStyles(() => ({
+export default makeStyles((theme) => ({
   appBar: {
     borderRadius: 15,
     margin: "30px 0",
@@ -14,5 +14,11 @@ export default makeStyles(() => ({
   },
   image: {
     marginLeft: "15px",
+  },
+  [theme.breakpoints.down("sm")]: {
+    //material-ui docs. this is for mobile ui
+    mainContainer: {
+      flexDirection: "column-reverse",
+    },
   },
 }));
