edf9bd101076e9487f40ebe31786cfbf828d9a0b
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-08-17 17:12:08 -0400

    input fields and auth setup

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,7 +1,58 @@
 import React from "react";
+import {
+  Avatar,
+  Button,
+  Paper,
+  Grid,
+  Typography,
+  Container,
+  TextField,
+} from "@material-ui/core";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import styles from "./styles";
 
 const Auth = () => {
-  return <div>AUTH</div>;
+  const classes = useStyles();
+  const isSignup = false;
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <Paper className={classes.paper} elevation={3}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography variant="h5">{isSignup ? "Sign up" : "Sign in"}</Typography>
+        <form className={classes.form} onSubmit={handleSubmit}>
+          <Grid container spacing={2}>
+            {isSignup && (
+              <>
+                <Grid xs={6} md={12}>
+                  <TextField
+                    name="firstName"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                    xs={6}
+                  />
+                </Grid>
+                <Grid xs={6} md={12}>
+                  <TextField
+                    name="firstName"
+                    label="First Name"
+                    handleChange={handleChange}
+                    autoFocus
+                    xs={6}
+                  />
+                </Grid>
+              </>
+            )}
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
+  );
 };
 
 export default Auth;
