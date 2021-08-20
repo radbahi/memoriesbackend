131aab2782f29d7065998ca55ce948358cbda427
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-08-20 14:05:52 -0400

    auth middleware done

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -62,16 +62,25 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
   const { id } = req.params;
 
+  if (!req.userId) return res.json({ message: "Unauthenticated" });
+
   if (!mongoose.Types.ObjectId.isValid(id))
     return res.status(404).send("No post with that ID");
 
   const post = await PostMessage.findById(id);
 
+  const index = post.likes.findIndex((id) => id === String(req.userId));
+
+  if (index === -1) {
+    // the -1 means the current id is not in that array of likes, so this is to push that id into that array of likes
+    post.likes.push(req.userId);
+  } else {
+    post.likes = post.likes.filter((id) => id !== String(req.userId));
+  }
+
   const updatedPost = await PostMessage.findByIdAndUpdate(
     id,
-    {
-      likeCount: post.likeCount + 1,
-    },
+    post,
     { new: true } // why do we do this?
   );
 
