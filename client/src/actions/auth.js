83b41bf40b0dacf935128dded45dcd8922cb3989
Radouane Bahi <radouanebahi@Radouanes-MacBook-Air.local>
2021-08-26 10:28:12 -0400

    users now being created but name isnt getting populated in localStorage

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,10 +1,10 @@
-import * as api from "../api"; //import * imports everything
+import * as api from "../api/index.js"; //import * imports everything
 
 export const signin = (formData, history) => async (dispatch) => {
   try {
-    const { data } = await api.signIn(formData);
+    const data = await api.signIn(formData);
     console.log(data);
-    dispatch({ type: "AUTH", data: data });
+    dispatch({ type: "AUTH", data });
     history.push("/");
   } catch (error) {
     console.log(error);
@@ -13,10 +13,12 @@ export const signin = (formData, history) => async (dispatch) => {
 
 export const signup = (formData, history) => async (dispatch) => {
   try {
-    const { data } = await api.signUp(formData);
+    console.log(formData);
+
+    const data = await api.signUp(formData);
     console.log(data);
 
-    dispatch({ type: "AUTH", data: data });
+    dispatch({ type: "AUTH", data });
     history.push("/");
   } catch (error) {
     console.log(error);
